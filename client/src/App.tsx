e4da88d3078c797af6908714f9ddb701fdad0dee
got3 <41207926-got3@users.noreply.replit.com>
2025-04-14 16:09:47 +0000

    Restored to '4acc4b7045cb282c1e42f9ef92e95adaff76ad5c'
    Replit-Restored-To: 4acc4b7045cb282c1e42f9ef92e95adaff76ad5c

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,7 +11,6 @@ import Trainers from "@/pages/trainers";
 import Reports from "@/pages/reports";
 import Settings from "@/pages/settings";
 import Users from "@/pages/users";
-import Calendar from "@/pages/calendar";
 import AuthPage from "@/pages/auth-page";
 import MainLayout from "@/components/layouts/main-layout";
 import { AuthProvider } from "@/hooks/use-auth";
@@ -95,11 +94,6 @@ function Router() {
       <ProtectedRoute 
         path="/users" 
         component={Users} 
-      />
-
-      <ProtectedRoute 
-        path="/calendar" 
-        component={Calendar} 
       />
       
       {/* Fallback to 404 */}
